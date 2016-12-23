7ba5a92f74c73680e9a060b30a4b893d1e9693f3
Ian Rose <ian@ianrose.me>
2016-12-22 23:55:49 -0500

    init project

---
+++ index.js
@@ -0,0 +1,26 @@
+var Metalsmith = require('metalsmith')
+var markdown = require('metalsmith-markdown')
+var layouts = require('metalsmith-layouts')
+var sass = require('metalsmith-sass')
+var filedata = require('metalsmith-filedata')
+
+Metalsmith(__dirname)
+  .source('./src')
+  .destination('./build')
+  .use(sass({
+    outputDir: 'styles'
+  }))
+  .use(filedata({
+    pattern: ['styles/*.css', 'scripts/*.js'],
+    key: 'cssData'
+  }))
+  .use(markdown())
+  .use(layouts({
+    engine: 'handlebars',
+    directory: 'layouts',
+    default: 'default.hbs',
+    pattern: '**/*.html' 
+  }))
+  .build(function (err) {
+    if (err) throw err
+  })
